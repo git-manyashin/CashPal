df6cac281ba7b1aa161b0cdd81724279f11dbbba
git-manyashin <johnny.work.development@gmail.com>
2025-05-30 17:34:01 +0200

    some training table creation, crud, primary and foreign keys

---
+++ CH3_Constraints/L4_foreign_keys/main.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (
+  id,
+  name,
+  country_code
+) VALUES (
+  1,
+  'Jerry',
+  'US'
+);
+
+INSERT INTO users (
+  id,
+  name,
+  country_code
+) VALUES (
+  2,
+  'Amit',
+  'IND'
+);
