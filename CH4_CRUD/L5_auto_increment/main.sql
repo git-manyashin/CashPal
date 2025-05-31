c6c1d24e94a3bef474e96c3acccc4e9c7606ac32
git-manyashin <johnny.work.development@gmail.com>
2025-05-31 09:43:48 +0200

    test auto incrementation and where clause

---
+++ CH4_CRUD/L5_auto_increment/main.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Lance', 20, 'US', 'LanChr', 'bootdevisbest', false);
+
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Tiffany', 28, 'US', 'Tifferoon', 'autoincrement', true);
